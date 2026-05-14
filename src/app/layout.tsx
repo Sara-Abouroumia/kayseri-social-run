0ef4df4da1d409766fbae10ac7e1e5371799bf41
Sara Abouroumia <saraabouroumia@gmail.com>
2026-05-14 17:45:52 +0300

    Vercel Analytics Coomponent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <>
+    <Analytics/>
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
@@ -39,5 +42,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </>
   );
 }
